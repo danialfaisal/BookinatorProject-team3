47fac3054c634f87291b698901bb2e163e49d57d
swetha byluppala <sbyluppala@unomaha.edu>
2018-10-31 16:20:02 -0500

    test

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 
+
 class Category(models.Model):
     name = models.CharField(max_length=200,
                             db_index=True)
@@ -30,7 +31,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to='products/%Y/%m/%d',
                               blank=True)
     edition = models.CharField(max_length=5, default='1st')
-    isbn = models.CharField(max_length=14, default='000000000000')
+    isbn = models.CharField(max_length=14, default='000000000000')    #idjfgo
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
